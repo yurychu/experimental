00f02c62f4a6c93aca9b59ec41dcc1217fb4ee60
yurychu <yury_chugunov@mail.ru>
2020-11-07 15:29:04 +0300

    comments code and listen check

--- code_parts/src/client/RunClient.cpp
+++ code_parts/src/client/RunClient.cpp
@@ -16,11 +16,14 @@ void run_client_1()
     std::string raw_ip_address = "127.0.0.1";
     uint16_t port_num = 3333;
     boost::system::error_code ec;
+    // very useful, validate addresses
     asio::ip::address ip_address = asio::ip::address::from_string(raw_ip_address, ec);
     if (ec.value() != 0){
         std::cout << "Failed to parse IP address. Err code = " << ec.value() << ". Message: " << ec.message() << std::endl;
         return;
     }
+    // endpoint is ip address and port.
+    // designate server application here
     asio::ip::tcp::endpoint ep { ip_address, port_num };
 }
 
@@ -28,10 +31,14 @@ void run_client_1()
 void run_client_2()
 {
     asio::io_service ios{};
-    asio::ip::tcp protocol = asio::ip::tcp::v4();
-    asio::ip::tcp::socket sock { ios };
+    asio::ip::tcp::socket sock { ios }; // just create io operations
+    // from protocol tcp, no real in Operating System
+
+    asio::ip::tcp protocol = asio::ip::tcp::v4(); // protocol object
+    // data structure with settings
+
     boost::system::error_code ec;
-    sock.open(protocol, ec);
+    sock.open(protocol, ec); // open socket with protocol object, real create in Operation System
     if (ec.value() != 0){
         std::cout << "Failed to open socket. Err code: " << ec.value() << ". Message: " << ec.message() << std::endl;
         return;
@@ -41,14 +48,17 @@ void run_client_2()
 
 void run_client_3()
 {
-    std::string host = "example.com";
-    std::string port = "3333";
+    std::string host = "vk.com";
+    std::string port = "80"; // string, resolver::query parse it.
 
     asio::io_service ios{};
 
+    // query to dns
+    // port number for construct end points.
+    // numeric service - port as number, not specific protocol.
     asio::ip::tcp::resolver::query res_query {host, port, asio::ip::tcp::resolver::query::numeric_service};
 
-    asio::ip::tcp::resolver rslvr { ios };
+    asio::ip::tcp::resolver rslvr { ios };  // use io operations
 
     boost::system::error_code ec;
 
@@ -57,9 +67,10 @@ void run_client_3()
         std::cout << "Failed to resolve DNS name: " << ec.value() << ", Msg: " << ec.message() << std::endl;
         return;
     }
-    asio::ip::tcp::resolver::iterator it_end;
+    asio::ip::tcp::resolver::iterator it_end; // end iterator
     for (; it != it_end; ++it){
         asio::ip::tcp::endpoint ep = it->endpoint();
+        std::cout << ep.address().to_string() << std::endl;
     }
 
 }
@@ -67,7 +78,7 @@ void run_client_3()
 void run_client_4()
 {
     std::string raw_ip_addr = "127.0.0.1";
-    uint16_t port_num = 8002;
+    uint16_t port_num = 3333;
     boost::system::error_code ec;
 
     asio::ip::address ip_addr = asio::ip::address::from_string(raw_ip_addr, ec);
@@ -86,10 +97,13 @@ void run_client_4()
         return;
     }
 
-    sock.connect(ep, ec);
+    // we dont bind, but sock binded in connect().
+
+    sock.connect(ep, ec); // blocking running thread call
     if (ec){
         std::cout << "Fail connect to ep, code: " << ec.value() << ", msg: " << ec.message() << std::endl;
         return;
     }
+    // has also asio::connect(sock, it); iterate over resolved eps and try connect, no need open() before call, v4 or v6
 
 }
--- code_parts/src/server/RunServer.cpp
+++ code_parts/src/server/RunServer.cpp
@@ -1,6 +1,7 @@
 
 #include <RunServer.hpp>
 
+#include <thread>
 #include <iostream>
 #include <string>
 #include <cstdint>
@@ -11,12 +12,15 @@ namespace asio = boost::asio;
 
 void run_server_1()
 {
+    // protocol for listen incoming
     uint16_t port_num = 3333;
 
+    // all addresses
+    // need version v4 or v6
     asio::ip::address ip_addr = asio::ip::address_v6::any();
 
+    // endpoint represents all addresses on host
     asio::ip::tcp::endpoint ep { ip_addr, port_num };
-
 
 }
 
@@ -24,7 +28,7 @@ void run_server_2()
 {
     asio::io_service ios;
     asio::ip::tcp protocol = asio::ip::tcp::v6();
-    asio::ip::tcp::acceptor acc { ios };
+    asio::ip::tcp::acceptor acc { ios }; // passive socket
     boost::system::error_code ec;
     acc.open(protocol, ec);
     if (ec.value() != 0){
@@ -51,12 +55,18 @@ void run_server_3()
         return;
     }
 
-    acc.bind(ep, ec);
+    acc.bind(ep, ec);  // OS still reject connections requests. Need to listen mode switch.
     if (ec.value() != 0){
         std::cout << "Fail bind acc to ep, code: " << ec.value() << ", msg: " << ec.message() << std::endl;
         return;
     }
 
-    std::cout << "ep.addr() to string: " << ep.address().to_string() << std::endl;
+    const int BACKLOG_SIZE = 20;
+    acc.listen(BACKLOG_SIZE); // switch acc socket to listen mode, OS not rejects requests here.
 
+    std::cout << "To sleep for ..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds{99});
+
+
+    std::cout << "End." << std::endl;
 }
